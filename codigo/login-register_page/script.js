682aa41058b972f3d934f7ff6200597f5c4679c1
João Silva <mtheu.gbiel.odr@gmail.com>
2022-04-21 17:29:42 -0300

    Adiciona login/register

---
+++ codigo/login-register_page/script.js
@@ -0,0 +1,11 @@
+function showLoginForm() {
+  //excluindo o formulário antigo
+  var elem = document.getElementById("form");
+  elem.parentNode.removeChild(elem);
+}
+
+function showRegisterForm() {
+  //excluindo o formulário antigo
+  var elem = document.getElementById("form");
+  elem.parentNode.removeChild(elem);
+}
